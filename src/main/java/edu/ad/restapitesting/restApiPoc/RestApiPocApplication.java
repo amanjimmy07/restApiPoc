0b8ddf6c7bb6016d341bd108129ab88829d96476
Aman Dubey <Aman Dubey@192.168.1.4>
2019-03-16 16:10:17 +0530

    Initial commit for Project setup.

---
+++ src/main/java/edu/ad/restapitesting/restApiPoc/RestApiPocApplication.java
@@ -0,0 +1,13 @@
+package edu.ad.restapitesting.restApiPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ad/restapitesting/restApiPoc/RestApiPocApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ad.restapitesting.restApiPoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApiPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
